81cf7bc0e7f5cc088f0285a6c5518dbcad7774c0
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-09-01 20:35:57 +0800

    init

---
+++ Patches/LocalViewRouter_Patch.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using Kitchen;
+using UnityEngine;
+
+namespace KitchenLayoutPreviewTweaker.Patches
+{
+    [HarmonyPatch]
+    static class LocalViewRouter_Patch
+    {
+        [HarmonyPatch(typeof(LocalViewRouter), "GetPrefab")]
+        [HarmonyPostfix]
+        static void GetPrefab_Postix(ViewType view_type, ref GameObject __result)
+        {
+            if (view_type == ViewType.LayoutInfo && __result != null && __result.GetComponent<RotationLock>() == null)
+            {
+                RotationLock rotationLock = __result.AddComponent<RotationLock>();
+                rotationLock.PreferenceID = Main.LAYOUT_INFO_ROTATION_ID;
+                rotationLock.FixedRotation = Quaternion.Euler(0f, 43f, 0f);
+
+                Transform layoutContainer = __result.transform.Find("Container")?.Find("Body")?.Find("Layout Container");
+                if (layoutContainer != null)
+                {
+                    rotationLock.RotationAnimator = layoutContainer.GetComponent<Animator>();
+                    rotationLock.LockedTransform = layoutContainer.Find("Container");
+                }
+            }
+        }
+    }
+}
---
+++ RotationLock.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace KitchenLayoutPreviewTweaker
+{
+    public class RotationLock : MonoBehaviour
+    {
+        public Transform LockedTransform;
+
+        public Animator RotationAnimator;
+
+        public Quaternion FixedRotation;
+
+        public string PreferenceID;
+
+        private bool WasLocked = false;
+
+        void Update()
+        {
+            bool shouldLock = ShouldLock() && LockedTransform != null;
+
+            if (RotationAnimator != null && shouldLock)
+            {
+                RotationAnimator.enabled = false;
+            }
+            else if (WasLocked)
+            {
+                RotationAnimator.enabled = true;
+            }
+
+            if (shouldLock)
+            {
+                LockedTransform.localRotation = FixedRotation;
+                LockedTransform.localScale = Vector3.one;
+            }
+            else if (WasLocked)
+            {
+                LockedTransform.localRotation = Quaternion.identity;
+                LockedTransform.localScale = Vector3.zero;
+            }
+        }
+
+        bool ShouldLock()
+        {
+            if (PreferenceID == null || PreferenceID == string.Empty)
+                return true;
+            return !Main.PrefManager?.Get<bool>(PreferenceID) ?? false;
+        }
+    }
+}
